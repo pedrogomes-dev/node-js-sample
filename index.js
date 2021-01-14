d136d83000025af414a51d275766288ee9e17aa7
PG <pedrogomes@detic.ro.gov.br>
2021-01-14 07:34:27 -0400

    definindo express + html

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var express = require('express')
+var fs = require('fs');
+var path = require('path');
+var ext = /[\w\d_-]+\.[\w\d]+$/;
+
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
